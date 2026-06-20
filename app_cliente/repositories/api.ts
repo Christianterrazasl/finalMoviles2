395a4b25af102eac32a040136e8a503b6894fe31
christianterrazasl <christianterrazas47@gmail.com>
2026-06-20 16:11:14 -0400

    env change

--- app_cliente/repositories/api.ts
+++ app_cliente/repositories/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = 'http://67.205.172.167';
+
 export const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_BASE_URL,
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
